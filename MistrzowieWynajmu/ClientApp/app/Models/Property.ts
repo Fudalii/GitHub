a81e3b7776620bb5c0a4b7809449b14a9a1c810d
Kamil Fudali <fudalikamil@hotmail.com>
2018-03-02 15:22:40 +0100

    Dodanie Modelu i serwisu Angular

---
+++ MistrzowieWynajmu/ClientApp/app/Models/Property.ts
@@ -0,0 +1,35 @@
+﻿
+export class Property {
+
+   public Id: number;
+   public PropertyType: Type;
+   public Description: string;
+   public Rooms: number;
+   public Area: number;
+   public Washer: boolean;
+   public Refrigerator: boolean;
+   public Iron: boolean;
+   //public Address: Address;
+   //public Owner: Owner;
+}
+
+  // enum Type {
+  //     House,
+  //     Flat
+  //      }
+
+
+  // class Owner {
+
+  //     OwnerId: number;
+  //     Name: string;
+  //     Surname: string;
+  //     Phone: string;
+  // }
+
+
+  // class Address {
+  //     AddressId: number;
+		//Street: string;
+		//City: string;
+  // }
---
+++ MistrzowieWynajmu/ClientApp/app/services/property-backend.service.ts
@@ -0,0 +1,18 @@
+﻿import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Property } from "../Models/Property";
+
+@Injectable()
+export abstract class BackendPropertyBackendServices {
+
+    abstract addProperty(newProperty: Property): Observable<number>;
+
+    abstract getProperty(id: number): Observable<Property>;
+
+    abstract getProperties(): Observable<Property[]>;
+
+    abstract updateProperty(updateProperty: Property): Observable<number>;
+
+    abstract deleteProperty(property: Property): Observable<number>;
+
+}
